eb12f432421160b7b6123d0a765f07f95bdb6008
lakshmipathireddydilli <lakshmipathireddydilli@gmail.com>
2022-09-27 22:09:47 +0530

    adding code for admin or user display edit add new -

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,7 +1,47 @@
-""" to avoid below flake erors we use # noqa beside import  
- ./core/admin.py:1:1: F401 'django.contrib.admin' imported but unused
 """
+Django admin customisation
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as translate
 
-from django.contrib import admin  # noqa
+from core import models
 
-# Register your models here.
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+    ordering = ['id']
+    list_display = ['email','name']
+    fieldsets = (
+        (None,{'fields':('email','password')}),
+        (
+            translate('Permission'),
+            {
+                'fields':(
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (
+            translate('Important dates'),{'fields':('last_login',)},
+        )
+
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets=(
+        (None,{
+            'classes':('wide',),
+            'fields':(
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }
+        ),
+    )
+admin.site.register(models.User, UserAdmin)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,43 @@
+"""Tests for Django admin modifications"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from django.test import Client
+
+class AdminSiteTests(TestCase):
+    """Tests for Django Admin"""
+
+    def setUp(self):
+        """ Create user and client"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@example.com',
+            password= 'testpass123'
+        )
+        # forces to login with super user details
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email = 'testuser@example.com',
+            password = 'test123'
+        )
+
+    def test_users_list(self):
+        """Test the users are listed on page """
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+        self.assertContains(res,self.user.email)
+        self.assertContains(res,self.user.name)
+
+    def test_edit_user_page(self):
+        """Test by change user details"""
+        url = reverse('admin:core_user_change',args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_add_user_page(self):
+        """ Test to create user page """
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code,200)
