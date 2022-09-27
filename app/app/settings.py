66707ca40bbe0e6c628d1516ac065267a8fefb37
lakshmipathireddydilli <lakshmipathireddydilli@gmail.com>
2022-09-27 22:42:07 +0530

    added api documentation settings and paths

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'rest_framework',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # setting Auth_User_Model Configuration
 AUTH_USER_MODEL = 'core.User'
+
+# configuration for schema documentation to do
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS':'drf_spectacular.openapi.AutoSchema',
+}
